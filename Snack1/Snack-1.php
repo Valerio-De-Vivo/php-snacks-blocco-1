1b9ac3734bdc4e305a64e874146e2ebdc8da0592
Valerio <valeriodevivo90@gmail.com>
2021-02-25 16:14:16 +0100

    Finish Snack 1

--- Snack1/Snack-1.php
+++ Snack1/Snack-1.php
@@ -1,3 +1,45 @@
+<?php 
+    $matches = [
+        [
+            "sc" => 'Juventus',
+            "so" => 'Lazio',
+            "pc" => 2,
+            "po" => 3
+        ],
+        [
+            "sc" => 'Inter',
+            "so" => 'Roma',
+            "pc" => 3,
+            "po" => 1
+        ],
+        [
+            "sc" => 'Atalanta',
+            "so" => 'Milan',
+            "pc" => 4,
+            "po" => 4
+        ],
+        [
+            "sc" => 'Sassuolo',
+            "so" => 'Spal',
+            "pc" => 0,
+            "po" => 1
+        ],
+        [
+            "sc" => 'Crotone',
+            "so" => 'Fiorentina',
+            "pc" => 1,
+            "po" => 1
+        ],
+        [
+            "sc" => 'Verona',
+            "so" => 'Potenza',
+            "pc" => 0,
+            "po" => 2
+        ],
+    ];
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,5 +50,24 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+
+    <?php 
+        for ($i = 0; $i < count($matches); $i++) { ?>
+            
+            <div class="matches">
+                <div class="squadre">
+                <h2> <span>casa:</span> <?php echo $matches[$i]["sc"] ?></h2>
+                -
+                <h2> <span>ospite:</span> <?php echo $matches[$i]["so"] ?> </h2>
+                </div>
+                <div class="punti">
+                    <h2><?php echo $matches[$i]["pc"] ?> </h2>
+                    -
+                    <h2><?php echo $matches[$i]["po"] ?> </h2>
+                </div>
+            </div>
+       <?php } ?> 
+
+
 </body>
 </html>
